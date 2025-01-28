d4065e48df903f99f365ddc5c1f1c83942388eb2
Ishant Batra <109754045+idk-12345678@users.noreply.github.com>
2025-01-28 08:48:10 -0700

    Swerve should almost work now except for the constants

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,11 +4,38 @@
 
 package frc.robot.subsystems;
 
+import java.io.File;
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import frc.robot.*;
+import swervelib.SwerveDrive;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
+
+double maximumSpeed = Units.feetToMeters(Constants.DriveConstants.maxSpeed);
+File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
   public DriveSubsystem() {
-    
+    try{
+    SwerveDrive swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed);
+    }
+    catch(Exception e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public Command getDriveCommand(double translationX, double translationY, double angularRotationX)
+  {
+    return run(() -> {
+      swerveDrive.drive(new Translation2d(translationX * swerveDrive.getMaximumVelocity(),
+                                          translationY * swerveDrive.getMaximumVelocity()),
+                        angularRotationX * swerveDrive.getMaximumAngularVelocity(),
+                        true,
+                        false);
+    });
   }
 
   @Override
