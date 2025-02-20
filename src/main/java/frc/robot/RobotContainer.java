7b4f8cf34f7f02ae48d683cd6c758a9b67aae959
team8885 <maeserrobotics@gmail.com>
2025-02-19 18:43:54 -0700

    e

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import swervelib.SwerveInputStream;
 //import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
@@ -22,14 +23,24 @@ public class RobotContainer {
   
   public static final CommandJoystick m_driverController = new CommandJoystick(OperatorConstants.kDriverControllerPort);
 
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(driveSubsystem.getSwerveDrive(),
+  () -> m_driverController.getY() * -1,
+  () -> m_driverController.getX() * -1)
+.withControllerRotationAxis(() -> m_driverController.getX())
+.deadband(0)
+.scaleTranslation(0.8)
+.allianceRelativeControl(true);
 
+  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(() -> m_driverController.getX(),
+                                                                                             () -> m_driverController.getY())
+                                                           .headingWhile(true);
   public RobotContainer() {
     configureBindings();
     exampleSubsystem = new ExampleSubsystem();
   }
 
   private void configureBindings() {
-    driveSubsystem.setDefaultCommand(driveSubsystem.getDriveCommand());
+    driveSubsystem.setDefaultCommand(driveSubsystem.driveFieldOriented(driveDirectAngle));
 
     
   }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,8 +5,7 @@
 package frc.robot.subsystems;
 
 import java.io.File;
-
-
+import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj.Filesystem;
 import swervelib.parser.SwerveParser;
@@ -17,6 +16,7 @@ import swervelib.SwerveDrive;
 import swervelib.SwerveModule;
 import swervelib.math.SwerveMath;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -52,15 +52,25 @@ File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
                         isFieldRelative,
                         false);
     };
+
+    
 //change field relativity based on driver preference
   public Command getDriveCommand(){
     return this.run(() -> {drive(RobotContainer.m_driverController.getY(), RobotContainer.m_driverController.getX(), RobotContainer.m_driverController.getTwist(), !!true);});
   }
 
   
+  public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity)
+  {
+    return run(() -> {
+      swerveDrive.driveFieldOriented(velocity.get());
+    });
+  }
 
   
-
+  public SwerveDrive getSwerveDrive(){
+    return this.swerveDrive;
+  }
 
   @Override
   public void periodic() {
