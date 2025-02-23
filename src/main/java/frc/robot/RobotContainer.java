399e5895aa889b2ce5ab3814adf0fc89c3563a19
team8885 <maeserrobotics@gmail.com>
2025-02-22 18:02:27 -0700

    yippie almost done

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,8 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
-import swervelib.SwerveInputStream;
-//import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.commands.*;
@@ -17,23 +15,11 @@ import frc.robot.commands.*;
 public class RobotContainer {
   // subsystems and commands are defined here...
   DriveSubsystem driveSubsystem = new DriveSubsystem();
-
-
   ExampleSubsystem exampleSubsystem;
   
   public static final CommandJoystick m_driverController = new CommandJoystick(OperatorConstants.kDriverControllerPort);
 
-  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(driveSubsystem.getSwerveDrive(),
-  () -> m_driverController.getY() * -1,
-  () -> m_driverController.getX() * -1)
-.withControllerRotationAxis(() -> m_driverController.getX())
-.deadband(0)
-.scaleTranslation(0.8)
-.allianceRelativeControl(true);
 
-  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(() -> m_driverController.getX(),
-                                                                                             () -> m_driverController.getY())
-                                                           .headingWhile(true);
   public RobotContainer() {
     configureBindings();
     exampleSubsystem = new ExampleSubsystem();
@@ -41,8 +27,6 @@ public class RobotContainer {
 
   private void configureBindings() {
     driveSubsystem.setDefaultCommand(driveSubsystem.getDriveCommand());
-
-    
   }
 
 
@@ -50,4 +34,3 @@ public class RobotContainer {
     return new ExampleCommand(exampleSubsystem);
   }
 }
-
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,10 +4,9 @@
 
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Meter;
-
 import java.io.File;
-import java.util.function.Supplier;
+
+
 
 import edu.wpi.first.wpilibj.Filesystem;
 import swervelib.parser.SwerveParser;
@@ -17,10 +16,7 @@ import frc.robot.*;
 import swervelib.SwerveDrive;
 import swervelib.SwerveModule;
 import swervelib.math.SwerveMath;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -30,69 +26,43 @@ public class DriveSubsystem extends SubsystemBase {
 
 double maximumSpeed = Units.feetToMeters(Constants.DriveConstants.maxSpeed);
 File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
-
-SwerveModule[] modules;
-
   SwerveDrive swerveDrive;
   public DriveSubsystem() {
-    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
     try{
-      swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(Units.feetToMeters(maximumSpeed), new Pose2d(new Translation2d(Meter.of(0), Meter.of(4)), Rotation2d.fromDegrees(0)));
-      modules = swerveDrive.getModules();
-      }
-    catch(Exception e){
-      throw new RuntimeException(e);
-    }
-
-    swerveDrive.setHeadingCorrection(false);
-    swerveDrive.setCosineCompensator(false);
-    swerveDrive.setAngularVelocityCompensation(true,
-    true,
-    0.1);
-    swerveDrive.setModuleEncoderAutoSynchronize(false,
-    1);
-
-
-  }
-
-  public Command driveCommand(double translationX, double translationY, double angularRotationX)
-  {
-    return run(() -> {
-      // Make the robot move
-      swerveDrive.drive(SwerveMath.scaleTranslation(new Translation2d(
-                            translationX * swerveDrive.getMaximumChassisVelocity(),
-                            translationY * swerveDrive.getMaximumChassisVelocity()), 0.8),
-                        Math.pow(angularRotationX, 3) * swerveDrive.getMaximumChassisAngularVelocity(),
-                        false,
-                        false);
-    });
-  }
-
-    
-//change field relativity based on driver preference
-  public Command getDriveCommand(){
-    return this.run(() -> {driveCommand(RobotContainer.m_driverController.getY(), RobotContainer.m_driverController.getX(), RobotContainer.m_driverController.getTwist());});
-  }
-
-  
-  public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity)
-  {
-    return run(() -> {
-      swerveDrive.driveFieldOriented(velocity.get());
-    });
-  }
-
-  
-  public SwerveDrive getSwerveDrive(){
-    return this.swerveDrive;
-  }
-
-  @Override
-  public void periodic() {
+      swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(Units.feetToMeters(maximumSpeed));
+      SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+      SwerveModule[] modules = swerveDrive.getModules();
       for(SwerveModule m: modules){
         m.getAngleMotor().setMotorBrake(true);
         m.getDriveMotor().setMotorBrake(true);
       }
+      }
+    catch(Exception e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void drive(double translationX, double translationY, double angularRotationX, boolean isFieldRelative)
+  {
+
+   swerveDrive.drive(SwerveMath.scaleTranslation(new Translation2d(
+                            translationX * swerveDrive.getMaximumChassisVelocity(),
+                            translationY * swerveDrive.getMaximumChassisVelocity()), 0.8),
+                        Math.pow(angularRotationX, 3) * swerveDrive.getMaximumChassisAngularVelocity(),
+                        isFieldRelative,
+                        false);
+    };
+//change field relativity based on driver preference
+  public Command getDriveCommand(){
+    return this.run(() -> {drive(-RobotContainer.m_driverController.getY(), RobotContainer.m_driverController.getX(), RobotContainer.m_driverController.getTwist(), true);});
+  }
+
+  
+
+
+  @Override
+  public void periodic() {
+
   }
 
 }
