d5f12af242defdccd300eb1883187d1ad21bf8b8
Ishant Batra <ishantpbatra1234@gmail.com>
2025-03-04 08:41:07 -0700

    Added some auto stuff

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -6,8 +6,12 @@ package frc.robot.subsystems;
 
 import java.io.File;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
-
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import swervelib.parser.SwerveParser;
@@ -17,7 +21,9 @@ import frc.robot.*;
 import swervelib.SwerveDrive;
 import swervelib.SwerveModule;
 import swervelib.math.SwerveMath;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -27,6 +33,7 @@ double maximumSpeed = Units.feetToMeters(Constants.DriveConstants.maxSpeed);
 File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
   SwerveDrive swerveDrive;
   boolean fieldRel;
+  RobotConfig config;
   public DriveSubsystem() {
     try{
       swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(Units.feetToMeters(maximumSpeed));
@@ -40,18 +47,66 @@ File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
     catch(Exception e){
       throw new RuntimeException(e);
     }
+
     fieldRel = true;
   }
 
-  public void drive(double translationX, double translationY, double angularRotationX, boolean isFieldRelative)
-  {
+  public void setUpAuto(){
+    //config for auto
+    try{
+      config = RobotConfig.fromGUISettings();
+      final boolean enableFeedforward = true; 
+      //build auto
+     AutoBuilder.configure(
+      this::getPose, // Robot pose supplier
+      this::resetPose, // Method to reset odometry 
+      this::getCurrentSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+      (speeds, feedforwards) -> {
+        if (enableFeedforward)
+        {
+          swerveDrive.drive(
+              speeds,
+              swerveDrive.kinematics.toSwerveModuleStates(speeds),
+              feedforwards.linearForces()
+                           );
+        } else
+        {
+          swerveDrive.setChassisSpeeds(speeds);
+        }
+      }, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. 
+      new PPHolonomicDriveController(
+              new PIDConstants(0.01, 0.0, 0.0), // drivePID
+              new PIDConstants(0.01, 0.0, 0.0) // rotatePID
+      ),
+      config, 
+      () -> {
+        // Boolean supplier that controls when the path will be mirrored for the red alliance
+        // This will flip the path being followed to the red side of the field.
+        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      this 
+);
+    } catch (Exception e) {
+      e.printStackTrace();
+    } 
+    
+  }
+
+  public Command drive(double translationX, double translationY, double angularRotationX, boolean isFieldRelative)
+  {
+    return this.run(() -> {
    swerveDrive.drive(SwerveMath.scaleTranslation(new Translation2d(
                             translationX * swerveDrive.getMaximumChassisVelocity(),
                             translationY * swerveDrive.getMaximumChassisVelocity()), 0.8),
                         Math.pow(angularRotationX, 3) * swerveDrive.getMaximumChassisAngularVelocity(),
                         isFieldRelative,
-                        false);
+                        false);});
     }
 
     public Command switchFieldRel(){
@@ -60,8 +115,21 @@ File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
   }
 //change field relativity based on driver preference
   public Command getDriveCommand(){
-    return this.run(() -> {drive(-RobotContainer.m_driverController.getY(), -RobotContainer.m_driverController.getX(), -RobotContainer.m_driverController.getTwist(), fieldRel);});
+    return drive(-RobotContainer.m_driverController.getY(), -RobotContainer.m_driverController.getX(), -RobotContainer.m_driverController.getTwist(), fieldRel);
   }
 
+  public Pose2d getPose(){
+    return swerveDrive.getPose();
+  }
+
+  public void resetPose(Pose2d initialPose)
+  {
+    swerveDrive.resetOdometry(initialPose);
+  }
+
+    public ChassisSpeeds getCurrentSpeeds()
+  {
+    return swerveDrive.getRobotVelocity();
+  }
 
 }
