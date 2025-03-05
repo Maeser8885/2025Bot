dbc53048d90ad7ef6a60433d0b2092ca0244c22e
Ishant Batra <109754045+idk-12345678@users.noreply.github.com>
2025-03-05 08:48:31 -0700

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -53,7 +53,6 @@ File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
   }
 
   public void setUpAuto(){
-    //BRING YOUR CAMERA TO CALCULUS
     //config for auto
     try{
       config = RobotConfig.fromGUISettings();
