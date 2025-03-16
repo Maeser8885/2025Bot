adf6e6e289e1e46562c38a285edd3d1276f9890c
JustAPenguin <62674346+char-cotton@users.noreply.github.com>
2025-03-16 14:49:33 -0600

    Removed Unused Imports

--- src/main/java/frc/robot/controlschemes/ExampleControlScheme.java
+++ src/main/java/frc/robot/controlschemes/ExampleControlScheme.java
@@ -1,11 +1,5 @@
 package frc.robot.controlschemes;
 
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.GrabberSubsystem;
-
 public class ExampleControlScheme extends ControlScheme {
 
     String name = "Drive Only";
--- src/main/java/frc/robot/controlschemes/StandardControlScheme.java
+++ src/main/java/frc/robot/controlschemes/StandardControlScheme.java
@@ -1,12 +1,7 @@
 package frc.robot.controlschemes;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.GrabberSubsystem;
 
 public class StandardControlScheme extends ControlScheme{
 
